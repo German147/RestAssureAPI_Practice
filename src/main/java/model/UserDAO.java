7e0ad222b5e1589e368e5716fca0d761e30dec13
german147 <barreragerman27@gmail.com>
2025-04-14 17:27:32 -0300

    It was refactored some test functions at the constructor level

--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -7,30 +7,53 @@ import exceptions.GetUserException;
 import java.sql.*;
 
 public class UserDAO {
-
     public User getUserById(int id) {
         User user = null;
 
-        try (Connection conn = DBConnection.getInstance()){
-            String query = "SELECT * FROM users WHERE id = " + id;
+        try (Connection conn = DBConnection.getInstance()) {
+            String query = """
+            SELECT u.id AS user_id, u.name, u.username, u.email,
+                   a.street, a.suite, a.city, a.zipcode,
+                   a.lat, a.lng
+            FROM users u
+            JOIN addresses a ON u.address_id = a.id
+            WHERE u.id = """ + id;
 
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(query);
 
             if (rs.next()) {
+                // Build Geo object
+                Geo geo = new Geo(
+                        rs.getString("lat"),
+                        rs.getString("lng")
+                );
+
+                // Build Address object
+                Address address = new Address(
+                        rs.getString("street"),
+                        rs.getString("suite"),
+                        rs.getString("city"),
+                        rs.getString("zipcode"),
+                        geo
+                );
+
+                // Build User object
                 user = new User(
-                        rs.getInt("id"),
+                        rs.getInt("user_id"),
                         rs.getString("name"),
                         rs.getString("username"),
-                        rs.getString("email")
+                        rs.getString("email"),
+                        address
                 );
             }
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new GetUserException("Failed to get user by id");
         }
 
         return user;
     }
+
 
 }
